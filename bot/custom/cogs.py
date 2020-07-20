8aa13308890b30471cad1d43e12483480d33be99
Edaz <edazpotato@gmail.com>
2020-07-21 11:29:22 +1200

    Update cogs.py

--- bot/custom/cogs.py
+++ bot/custom/cogs.py
@@ -9,11 +9,11 @@ class CogLoader:
         # initialize cogs
         # event listeners
         self.bot.add_cog(eventListeners.Events(self.bot))
-        # loops
-        self.bot.add_cog(httpLoops.Loop(self.bot))
         # commands
         self.bot.add_cog(adminCommands.Admin(self.bot))
         self.bot.add_cog(moderationCommands.Moderation(self.bot))
         self.bot.add_cog(utilityCommands.Utility(self.bot))
         self.bot.add_cog(funCommands.Fun(self.bot))
         self.bot.add_cog(minecraftCommands.Minecraft(self.bot))
+        # loops
+        self.bot.add_cog(httpLoops.Loop(self.bot))
