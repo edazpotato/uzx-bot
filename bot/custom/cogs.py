74d551c7c4a129b294f6e0fe96c8839134069625
Edaz Potato <edazpotato@gmail.com>
2020-07-21 20:08:27 +1200

    fix thigns that git broke

--- bot/custom/cogs.py
+++ bot/custom/cogs.py
@@ -7,19 +7,13 @@ class CogLoader:
         self.bot = bot
         # Load cogs
         self.bot.add_cog(eventListeners.Events(self.bot))
-<<<<<<< HEAD
         self.bot.add_cog(httpLoops.Loop(self.bot))
-=======
         # commands
->>>>>>> 0e329c2b7fb03bf3c1a2aa1a6c660c9073e1dd9b
         self.bot.add_cog(adminCommands.Admin(self.bot))
         self.bot.add_cog(moderationCommands.Moderation(self.bot))
         self.bot.add_cog(utilityCommands.Utility(self.bot))
         self.bot.add_cog(funCommands.Fun(self.bot))
         self.bot.add_cog(minecraftCommands.Minecraft(self.bot))
-<<<<<<< HEAD
 
-=======
         # loops
         self.bot.add_cog(httpLoops.Loop(self.bot))
->>>>>>> 0e329c2b7fb03bf3c1a2aa1a6c660c9073e1dd9b
